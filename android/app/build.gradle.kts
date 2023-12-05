6eaee137fda417875e465242222a9fb3894f231a
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-12-05 19:27:13 +0900

    setting :: gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.greenonion.astario"
-    compileSdk = 33
+    namespace = "com.personal.astario"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.greenonion.astario"
+        applicationId = "com.personal.astario"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
@@ -39,11 +40,15 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
