34c55783711967b5275cc787b2bafb8c138430e4
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-12-23 23:38:30 +0900

    delete :: comment

--- android/app/src/main/java/com/personal/astario/FortuneFragment.kt
+++ android/app/src/main/java/com/personal/astario/FortuneFragment.kt
@@ -35,12 +35,6 @@ class FortuneFragment : Fragment() {
         binding.recyclerFortune.layoutManager = LinearLayoutManager(requireContext())
     }
 
-    /*private val starList: ArrayList<String> = arrayListOf(
-        "물병자리", "물고기자리", "양자리", "황소자리",
-        "쌍둥이자리", "게자리", "사자자리", "처녀자리",
-        "천칭자리", "전갈자리", "사수자리", "염소자리"
-    )*/
-
     private fun getFBContentData() {
         val postListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
